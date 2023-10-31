3351db180b0c3dcb185b21b9b9c0afd4f5b8f9ba
eebydeeby <86206723+eebydeeby@users.noreply.github.com>
2023-10-31 07:48:12 +0000

    Game over condition added

--- Assets/Scripts/FruitSpawner.cs
+++ Assets/Scripts/FruitSpawner.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class FruitSpawner : MonoBehaviour
 {
@@ -40,7 +41,7 @@ public class FruitSpawner : MonoBehaviour
 			{
 				timerIsRunning = false;
 				Destroy(gameObject);
-				Debug.Log("Game over!");
+				SceneManager.LoadScene("GameOver", LoadSceneMode.Single);
 			}
 		}
 	}
